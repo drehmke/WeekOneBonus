d4fda19a8b4d0624ca430c9b24995a5c03a867e2
Donna Ryan <donna@nerdrose.com>
2016-10-18 17:40:55 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var myArray = [true, true, true, true, true, true, true, true, true, true];
+
+for( i = null; i <= myArray.length; i++ ){
+  if( i === null )  {}
+  else              { console.log(i); }
+
+}
